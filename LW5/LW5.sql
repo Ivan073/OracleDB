fa06b02c2d3919f6c000aa487a8c748802951400
Ivan <vanya_zhechko@tut.by>
2024-05-08 17:12:46 +0300

    Rollback procedures

--- LW5/LW5.sql
+++ LW5/LW5.sql
@@ -33,6 +33,7 @@ CREATE TABLE Students(
 --2 Table logs
 DROP Table StudentLog;
 CREATE Table StudentLog(
+    id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     operation varchar2(100),
     st_id number NOT NULL,
     st_name varchar2(200),
@@ -43,6 +44,7 @@ CREATE Table StudentLog(
 
 DROP Table GroupLog;
 CREATE Table GroupLog(
+    id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     operation varchar2(100),
     gr_id number NOT NULL,
     gr_name varchar2(200),
@@ -53,6 +55,7 @@ CREATE Table GroupLog(
 
 DROP Table FacultyLog;
 CREATE Table FacultyLog(
+    id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     operation varchar2(100),
     f_id number NOT NULL,
     f_name varchar2(200),
@@ -124,6 +127,165 @@ END;
 
 
 
+--3 Package procedures
+CREATE OR REPLACE PACKAGE log_package AS
+  PROCEDURE rollback_tables(rollback_time IN TIMESTAMP);
+  PROCEDURE rollback_tables(rollback_interval IN INTERVAL DAY TO SECOND);
+END log_package;
+/
+
+
+
+CREATE OR REPLACE PACKAGE BODY log_package AS
+  PROCEDURE rollback_tables(rollback_time IN TIMESTAMP) IS
+     st_log_rec StudentLog%ROWTYPE;
+     gr_log_rec GroupLog%ROWTYPE;
+     f_log_rec FacultyLog%ROWTYPE;
+  BEGIN
+    FOR log_rec IN ( -- subquery for all revertable operations
+        SELECT id, operation, operation_date, 'Students' as TableName
+        FROM StudentLog
+        WHERE operation_date >= rollback_time
+        UNION
+        SELECT id, operation, operation_date, 'Groups' as TableName
+        FROM GroupLog
+        WHERE operation_date >= rollback_time
+        UNION
+        SELECT id, operation, operation_date, 'Faculties' as TableName
+        FROM FacultyLog
+        WHERE operation_date >= rollback_time
+        ORDER BY operation_date DESC
+    )
+    LOOP
+        IF log_rec.operation = 'Delete' THEN
+            IF log_rec.TableName = 'Students' THEN
+                SELECT * INTO st_log_rec FROM StudentLog WHERE id=log_rec.id;
+                INSERT INTO Students (id, name, admission_time, group_id)
+                VALUES (st_log_rec.st_id, st_log_rec.st_name, st_log_rec.st_admission_time, st_log_rec.st_group_id);
+            ELSIF log_rec.TableName = 'Groups' THEN
+                SELECT * INTO gr_log_rec FROM GroupLog WHERE id=log_rec.id;
+                INSERT INTO Groups (id, name, creation_time, faculty_id)
+                VALUES (gr_log_rec.gr_id, gr_log_rec.gr_name, gr_log_rec.gr_creation_time, gr_log_rec.gr_faculty_id);
+            ELSIF log_rec.TableName = 'Faculties' THEN
+                SELECT * INTO f_log_rec FROM FacultyLog WHERE id=log_rec.id;
+                INSERT INTO Faculties (id, name,creation_time)
+                VALUES (f_log_rec.f_id, f_log_rec.f_name, f_log_rec.f_creation_time);
+            END IF;
+        ELSIF log_rec.operation = 'Insert' THEN
+            IF log_rec.TableName = 'Students' THEN
+                SELECT * INTO st_log_rec FROM StudentLog WHERE id=log_rec.id;
+                DELETE FROM Students 
+                WHERE id = st_log_rec.st_id AND name = st_log_rec.st_name
+                AND group_id = st_log_rec.st_group_id AND admission_time = st_log_rec.st_admission_time;
+            END IF;
+            IF log_rec.TableName = 'Groups' THEN
+                SELECT * INTO gr_log_rec FROM GroupLog WHERE id=log_rec.id;
+                DELETE FROM Groups 
+                WHERE id = gr_log_rec.gr_id AND name = gr_log_rec.gr_name
+                AND faculty_id = gr_log_rec.gr_faculty_id AND creation_time = gr_log_rec.gr_creation_time;
+            END IF;
+            IF log_rec.TableName = 'Faculties' THEN
+                SELECT * INTO f_log_rec FROM FacultyLog WHERE id=log_rec.id;
+                DELETE FROM Faculties
+                WHERE id = f_log_rec.f_id AND name = f_log_rec.f_name
+                AND creation_time = f_log_rec.f_creation_time;
+            END IF;
+        END IF;
+    END LOOP;
+    
+    DELETE FROM StudentLog
+    WHERE operation_date >= rollback_time;
+    DELETE FROM GroupLog
+    WHERE operation_date >= rollback_time;
+    DELETE FROM FacultyLog
+    WHERE operation_date >= rollback_time;
+END;
+
+  PROCEDURE rollback_tables(rollback_interval IN INTERVAL DAY TO SECOND) IS
+        st_log_rec StudentLog%ROWTYPE;
+         gr_log_rec GroupLog%ROWTYPE;
+         f_log_rec FacultyLog%ROWTYPE;
+         rollback_time TIMESTAMP;
+      BEGIN
+        rollback_time := CURRENT_TIMESTAMP - rollback_interval;
+        FOR log_rec IN ( -- subquery for all revertable operations
+            SELECT id, operation, operation_date, 'Students' as TableName
+            FROM StudentLog
+            WHERE operation_date >= rollback_time
+            UNION
+            SELECT id, operation, operation_date, 'Groups' as TableName
+            FROM GroupLog
+            WHERE operation_date >= rollback_time
+            UNION
+            SELECT id, operation, operation_date, 'Faculties' as TableName
+            FROM FacultyLog
+            WHERE operation_date >= rollback_time
+            ORDER BY operation_date DESC
+        )
+        LOOP
+            IF log_rec.operation = 'Delete' THEN
+                IF log_rec.TableName = 'Students' THEN
+                    SELECT * INTO st_log_rec FROM StudentLog WHERE id=log_rec.id;
+                    INSERT INTO Students (id, name, admission_time, group_id)
+                    VALUES (st_log_rec.st_id, st_log_rec.st_name, st_log_rec.st_admission_time, st_log_rec.st_group_id);
+                ELSIF log_rec.TableName = 'Groups' THEN
+                    SELECT * INTO gr_log_rec FROM GroupLog WHERE id=log_rec.id;
+                    INSERT INTO Groups (id, name, creation_time, faculty_id)
+                    VALUES (gr_log_rec.gr_id, gr_log_rec.gr_name, gr_log_rec.gr_creation_time, gr_log_rec.gr_faculty_id);
+                ELSIF log_rec.TableName = 'Faculties' THEN
+                    SELECT * INTO f_log_rec FROM FacultyLog WHERE id=log_rec.id;
+                    INSERT INTO Faculties (id, name,creation_time)
+                    VALUES (f_log_rec.f_id, f_log_rec.f_name, f_log_rec.f_creation_time);
+                END IF;
+            ELSIF log_rec.operation = 'Insert' THEN
+                IF log_rec.TableName = 'Students' THEN
+                    SELECT * INTO st_log_rec FROM StudentLog WHERE id=log_rec.id;
+                    DELETE FROM Students 
+                    WHERE id = st_log_rec.st_id AND name = st_log_rec.st_name
+                    AND group_id = st_log_rec.st_group_id AND admission_time = st_log_rec.st_admission_time;
+                END IF;
+                IF log_rec.TableName = 'Groups' THEN
+                    SELECT * INTO gr_log_rec FROM GroupLog WHERE id=log_rec.id;
+                    DELETE FROM Groups 
+                    WHERE id = gr_log_rec.gr_id AND name = gr_log_rec.gr_name
+                    AND faculty_id = gr_log_rec.gr_faculty_id AND creation_time = gr_log_rec.gr_creation_time;
+                END IF;
+                IF log_rec.TableName = 'Faculties' THEN
+                    SELECT * INTO f_log_rec FROM FacultyLog WHERE id=log_rec.id;
+                    DELETE FROM Faculties
+                    WHERE id = f_log_rec.f_id AND name = f_log_rec.f_name
+                    AND creation_time = f_log_rec.f_creation_time;
+                END IF;
+            END IF;
+        END LOOP;
+        
+        DELETE FROM StudentLog
+        WHERE operation_date >= rollback_time;
+        DELETE FROM GroupLog
+        WHERE operation_date >= rollback_time;
+        DELETE FROM FacultyLog
+        WHERE operation_date >= rollback_time;
+    END;
+END log_package;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -140,6 +302,16 @@ SELECT * FROM Students;
 
 
 
-INSERT INTO Faculties(Id,name,creation_time) VALUES (1,'faculty1',CURRENT_TIMESTAMP);
-INSERT INTO Groups(Id,name,creation_time,Faculty_id) VALUES (2,'group1',CURRENT_TIMESTAMP,1);
-INSERT INTO Students(Id,name,admission_time,Group_id) VALUES (1,'student1',CURRENT_TIMESTAMP,1);
+INSERT INTO Faculties(Id,name,creation_time) VALUES (3,'faculty1',CURRENT_TIMESTAMP);
+INSERT INTO Groups(Id,name,creation_time,Faculty_id) VALUES (3,'group1',CURRENT_TIMESTAMP,3);
+INSERT INTO Students(Id,name,admission_time,Group_id) VALUES (3,'student1',CURRENT_TIMESTAMP,3);
+
+
+BEGIN
+  log_package.rollback_tables(CURRENT_TIMESTAMP-200);
+END;
+
+
+BEGIN
+  log_package.rollback_tables(INTERVAL '200' SECOND);
+END;
